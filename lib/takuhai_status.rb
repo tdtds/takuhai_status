59d20c369a17078f998707d810dda5ba74616c9f
TADA Tadashi <t@tdtds.jp>
2015-07-02 17:23:59 +0900

    add TakuhaiStatus.scan method for searching key from all services

--- lib/takuhai_status.rb
+++ lib/takuhai_status.rb
@@ -3,5 +3,16 @@ require "takuhai_status/japanpost"
 require "takuhai_status/kuronekoyamato"
 
 module TakuhaiStatus
+	class NotFound < StandardError; end
 	class NotMyKey < StandardError; end
+
+	def self.scan(key)
+		[JapanPost, KuronekoYamato].each do |service|
+			begin
+				return service.new(key)
+			rescue NotMyKey
+			end
+		end
+		raise NotFound
+	end
 end
