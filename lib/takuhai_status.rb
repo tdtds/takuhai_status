d828919058cc4055d1a8e435b5b57b0d348c4cf7
TADA Tadashi <t@tdtds.jp>
2015-07-02 16:58:28 +0900

    add KuronekoYamato service

--- lib/takuhai_status.rb
+++ lib/takuhai_status.rb
@@ -1,5 +1,6 @@
 require "takuhai_status/version"
 require "takuhai_status/japanpost"
+require "takuhai_status/kuronekoyamato"
 
 module TakuhaiStatus
 	class NotMyKey < StandardError; end
---
+++ lib/takuhai_status/kuronekoyamato.rb
@@ -0,0 +1,34 @@
+require 'faraday'
+require 'nokogiri'
+
+module TakuhaiStatus
+	class KuronekoYamato
+		attr_reader :time, :state
+
+		def initialize(key)
+			@key = key.gsub(/[^0-9]/, '')
+			@time, @state = check
+		end
+
+	private
+		def check
+			conn = Faraday.new(url: 'http://toi.kuronekoyamato.co.jp/')
+			res = conn.post('/cgi-bin/tneko', {number00: '1', number01: @key})
+			doc = Nokogiri(res.body)
+
+			begin
+				tr = doc.css('.meisai')[0].css('tr').last
+				sday = tr.css('td')[2].text
+				stime = tr.css('td')[3].text
+				time = Time.parse("#{sday} #{stime}")
+				state = tr.css('td')[1].text
+
+				return time, state
+			rescue NoMethodError
+				raise NotMyKey
+			rescue ArgumentError
+				return Time.now, ''
+			end
+		end
+	end
+end
