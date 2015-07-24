22e065557103f3e11c12db60bbd2df3b0cbed335
TADA Tadashi <t@tdtds.jp>
2015-07-24 13:08:54 +0900

    fix invalid state in TMG

--- lib/takuhai_status/tmg_cargo.rb
+++ lib/takuhai_status/tmg_cargo.rb
@@ -31,12 +31,9 @@ module TakuhaiStatus
 			doc = Nokogiri(res.body)
 			begin
 				state = doc.css('#list tr td')[2].text.strip
-				raise NotMyKey if state =~ /お荷物情報が見つかりません/
 				return Time.now, state
-			rescue NotMyKey
-				raise
 			rescue
-				return Time.now, "(unknown)"
+				raise NotMyKey
 			end
 		end
 	end
