a4f8dd22b33c4de45223652725965788a34e3d1c
Kan Fushihara <kan.fushihara@gmail.com>
2015-10-05 21:37:10 +0900

    UPS http://www.ups.com/ の宅配ステータス取得に対応

--- lib/takuhai_status.rb
+++ lib/takuhai_status.rb
@@ -3,13 +3,14 @@ require "takuhai_status/japanpost"
 require "takuhai_status/kuronekoyamato"
 require "takuhai_status/sagawa"
 require "takuhai_status/tmg_cargo"
+require "takuhai_status/ups"
 
 module TakuhaiStatus
 	class NotFound < StandardError; end
 	class NotMyKey < StandardError; end
 
 	def self.scan(key)
-		[JapanPost, KuronekoYamato, Sagawa, TMGCargo].each do |service|
+		[JapanPost, KuronekoYamato, Sagawa, TMGCargo, UPS].each do |service|
 			begin
 				return service.new(key)
 			rescue NotMyKey
