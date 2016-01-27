5923855e49d92f25e5e7ebb95f1556c401ae6c8e
TADA Tadashi <t@tdtds.jp>
2016-01-27 21:51:28 +0900

    fix #6: support multiple services have same code
    * scan all services multi-threading
    * ignore finished services when multiple services found
    * raise Multiple error when multiple services found

--- lib/takuhai_status.rb
+++ lib/takuhai_status.rb
@@ -8,14 +8,42 @@ require "takuhai_status/ups"
 module TakuhaiStatus
 	class NotFound < StandardError; end
 	class NotMyKey < StandardError; end
+	class Multiple < StandardError
+		attr_reader :services
+		def initialize(msg, services)
+			super(msg)
+			@services = services
+		end
+	end
 
 	def self.scan(key)
-		[Sagawa, JapanPost, KuronekoYamato, TMGCargo, UPS].each do |service|
+		services = []
+		[].tap{|threads|
+			[Sagawa, JapanPost, KuronekoYamato, TMGCargo, UPS].each do |service|
+				threads.push(Thread.new{service.new(key)})
+			end
+		}.each{|thread|
 			begin
-				return service.new(key)
+				services.push(thread.value)
 			rescue NotMyKey
 			end
+		}
+
+		case services.size
+		when 0
+			raise NotFound
+		when 1
+			return services.first
+		else
+			services.delete_if{|service| service.finish?}
+			case services.size
+			when 0
+				raise NotFound
+			when 1
+				return services.first
+			else
+				raise Multiple.new('some services found', services)
+			end
 		end
-		raise NotFound
 	end
 end
