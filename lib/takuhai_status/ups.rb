a18f1a84b80b22fdcc48e253cc836ea2098c9e1b
TADA Tadashi <t@tdtds.jp>
2017-07-10 19:24:52 +0900

    fix error by changing site of UPS

--- lib/takuhai_status/ups.rb
+++ lib/takuhai_status/ups.rb
@@ -1,9 +1,11 @@
 require 'open-uri'
+require 'faraday'
 require 'nokogiri'
 
 module TakuhaiStatus
 	class UPS
 		attr_reader :key, :time, :state
+		@@conn = nil
 
 		def initialize(key)
 			@key = key.gsub(/[^a-zA-Z0-9]/, '')
@@ -16,18 +18,26 @@ module TakuhaiStatus
 
 	private
 		def check
-			uri = "https://www.ups.com/WebTracking/processInputRequest?loc=ja_JP&Requester=NES&tracknum=#{@key}"
-			html = open(uri, &:read)
-			# HTML中に謎の大量ヌル文字が含まれていてnokogiriのパースが止まる対策
-			html.gsub!(/\u0000/,'')
-			doc = Nokogiri::HTML.parse(html, uri, "utf-8")
+			@@conn = Faraday.new(url: 'https://wwwapps.ups.com/'){|builder|
+				builder.use :cookie_jar
+				builder.request :url_encoded
+				builder.adapter :net_http
+			} unless @@conn
+			@@conn.get('/WebTracking/track?loc=ja_JP') # once access to set cookie
+			res = @@conn.post('/WebTracking/track?loc=ja_JP', {
+				HTMLVersion: '5.0',
+				loc: 'ja',
+				trackNums: @key,
+				'track.x' => 'Track'
+			})
+			doc = Nokogiri::HTML.parse(res.body)
 
 			begin
 				state = doc.css('.newstatus h3')[0].text.strip
 				begin
-					time = "#{doc.css('.secHead ul li')[0].text.match(/\d{4}\/\d\d\/\d\d \d{1,2}:\d\d/)[0]}+0500)"
+					time = "#{doc.css('#fontControlButtons li li').text.match(/\d{4}\/\d\d\/\d\d \d{1,2}:\d\d/)[0]}+0500)"
 				rescue NoMethodError
-					time = "#{doc.css('.secHead ul li')[0].text.match(/\d{4}\/\d\d\/\d\d/)[0]} 00:00:00+0500)"
+					time = "#{doc.css('#fontControlButtons li').text.match(/\d{4}\/\d\d\/\d\d/)[0]} 00:00:00+0500)"
 				end
 				return Time.parse(time).localtime, state
 			rescue NoMethodError
