9d012fd48947dd7d181d38873f844237a6d59e29
TADA Tadashi <t@tdtds.jp>
2017-09-14 15:18:08 +0900

    test implement about global baggage tracking of KuronekoYamato
    * remove '国内到着' from finish state
    * check global state when '国内到着'
    * bump version to 1.6.0

--- lib/takuhai_status/kuronekoyamato.rb
+++ lib/takuhai_status/kuronekoyamato.rb
@@ -11,7 +11,7 @@ module TakuhaiStatus
 		end
 
 		def finish?
-			return !!(@state =~ /^(お客様引渡|配達|投函)完了|返品|国内到着/)
+			return !!(@state =~ /^(お客様引渡|配達|投函)完了|返品/)
 		end
 
 	private
@@ -27,12 +27,37 @@ module TakuhaiStatus
 				stime = tr.css('td')[3].text
 				time = Time.parse("#{sday} #{stime}")
 
+				if state == '国内到着'
+					begin
+						time, state = global_state(doc)
+					rescue
+						$stderr.puts "error in yamato global, about #{key}"
+					end
+				end
+
 				return time, state
 			rescue NoMethodError
 				raise NotMyKey
 			rescue ArgumentError
 				return Time.now, state || ''
 			end
+		end
+
+		def global_state(doc)
+			form = doc.css('form[method=POST]')
+			values = form.css('input[type=hidden]').map{|x| [x[:name], x[:value]]}.flatten
+			params = Hash[*values]
+
+			res = Faraday.new.post(
+				'http://globaltoi.kuronekoyamato.co.jp/Global/outside',
+				params
+			)
+			html = res.body.force_encoding('Shift_JIS').encode('UTF-8')
+			tr = Nokogiri(html).css('table.detail tr')
+			state = tr[tr.size-1].css('td')[1].text
+			loc = tr[tr.size-1].css('td')[4].text.strip.chop.chop
+			stime = "#{tr[tr.size-1].css('td')[2].text} #{tr[tr.size-1].css('td')[3].text}"
+			return Time.parse(stime), "#{state}[#{loc}]"
 		end
 	end
 end
