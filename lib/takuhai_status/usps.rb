5d07f654ad1a92f5ce2c7f0323b5cce7a50b69f5
TADA Tadashi <t@tdtds.jp>
2018-05-15 17:35:54 +0900

    support simply new service USPS

---
+++ lib/takuhai_status/usps.rb
@@ -0,0 +1,34 @@
+require 'faraday'
+require 'nokogiri'
+
+module TakuhaiStatus
+	class USPS
+		attr_reader :key, :time, :state
+		TakuhaiStatus.add_service(self)
+
+		def initialize(key)
+			@key = key.gsub(/[^a-zA-Z0-9]/, '')
+			@time, @state = check
+		end
+
+		def finish?
+			return !!(@state =~ /^Delivered/)
+		end
+
+	private
+		def check
+			url = "https://tools.usps.com/go/TrackConfirmAction?tLabels=#{@key}"
+			html = Faraday.new(url: url).get.body
+			date, status, detail = Nokogiri(html).css('div.status_feed p');
+			date_str = date.text.strip
+			status_str = status.text.gsub(/\u00A0/, ' ').strip
+			detail_str = detail.text.gsub(/\u00A0/, ' ').strip
+
+			unless date_str.empty?
+				return Time.parse(date_str), "#{status_str} (#{detail_str})"
+			else
+				raise NotMyKey
+			end
+		end
+	end
+end
