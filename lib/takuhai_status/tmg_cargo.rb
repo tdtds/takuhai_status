713139edde24152b431255b5227b827da6f98516
Tada, Tadashi <t@tdtds.jp>
2019-02-13 10:30:38 +0900

    Revert "follow new domain name of TMGCargo"
    This reverts commit becc691b7461f9e1fb321995e9e68db0a15a5d9c.

--- lib/takuhai_status/tmg_cargo.rb
+++ lib/takuhai_status/tmg_cargo.rb
@@ -20,7 +20,7 @@ module TakuhaiStatus
 
 	private
 		def check
-			@@conn = Faraday.new(url: 'https://track-a.tmg-tms.com'){|builder|
+			@@conn = Faraday.new(url: 'https://track-a.tmg-group.jp'){|builder|
 				builder.use :cookie_jar
 				builder.request :url_encoded
 				builder.adapter :net_http
